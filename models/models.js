908a3f32155f57d4ae85865b919846204740b93e
Marc!! <marc.bastien@gmail.com>
2015-05-12 03:56:45 +0000

    WHAT: Added Thing model to models.js

--- models/models.js
+++ models/models.js
@@ -4,17 +4,23 @@ var PersonSchema = new mongoose.Schema({
   active: { type: Boolean, default: true },
   color: { type: String, default: 'yellow' }
 });
-
 var Person = mongoose.model("Person", PersonSchema);
+
+var ThingSchema = new mongoose.Schema({
+  name: { type: String, unique: true, required: true },
+  color: { type: String, default: 'yellow' },
+  description: {type: String}
+});
+var Thing = mongoose.model("Thing", ThingSchema);
 
 var UserSchema = new mongoose.Schema({
   username: String,
   password: String
 });
-
 var User = mongoose.model("User", UserSchema);
 
 module.exports = {
+    Thing: Thing,
     Person: Person, 
     User: User
 };
